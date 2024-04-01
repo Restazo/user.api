f47bf38faa9f654e1a642a73d3e8b6d62cc46fb0
Matheus Oliveira <t2fema00@students.oamk.fi>
2024-04-01 19:23:09 +0300

    enhance: set global scope env variables

--- src/schemas/env.ts
+++ src/schemas/env.ts
@@ -2,6 +2,7 @@ import * as z from "zod";
 
 export const EnvSchema = z.object({
   ENV: z.string().min(1),
+  ASSETS_URL: z.string().min(1),
   API_PORT: z.string().min(1),
   USER_APP_URL: z.string().min(7),
   DB_USER: z.string().min(1),
