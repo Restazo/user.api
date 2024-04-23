b22a65c2982b9ad09220582fc6f4cf9971215acf
Matheus Oliveira <t2fema00@students.oamk.fi>
2024-04-24 02:31:12 +0300

    refactor: decipher function to return parsed JSON data instead of raw string

--- src/lib/decrypt.ts
+++ src/lib/decrypt.ts
@@ -2,15 +2,20 @@ import { createDecipheriv } from "crypto";
 
 const secret: Buffer = Buffer.from(process.env.ENCRYPTION_SECRET, "hex");
 
-const decrypt = (data: any): string => {
-  // create the decipher instance
-  const decipher = createDecipheriv("aes-256-ecb", secret, null);
-  // update the decipher with the encrypted data
-  let decrypted = decipher.update(data, "base64", "utf8");
-  // finalize the decryption process
-  decrypted += decipher.final("utf8");
-  // return the decrypted data
-  return decrypted;
+const decrypt = (data: any): unknown => {
+  try {
+    // create the decipher instance
+    const decipher = createDecipheriv("aes-256-ecb", secret, null);
+    // update the decipher with the encrypted data
+    let decrypted = decipher.update(data, "base64", "utf8");
+    // finalize the decryption process
+    decrypted += decipher.final("utf8");
+    // return the decrypted data
+    return JSON.parse(decrypted);
+  } catch (error) {
+    console.error("Decryption error: ", error);
+    return null;
+  }
 };
 
 export default decrypt;
